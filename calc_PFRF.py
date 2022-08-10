f3bb6a8cd45e10e15fbb2cff51c9e798a875726d
vladimir <vlgu@mail.ru>
2022-08-10 11:10:41 +0300

    добавил цвет кнопкам, заменил пустые массивы на пустые строки

--- calc_PFRF.py
+++ calc_PFRF.py
@@ -10,9 +10,9 @@ root.geometry('200x220+100+100')
 root.resizable(False, False)  # блокирует изменение размеров окна
 # root.iconbitmap('ico_calc_PFRF.ico') #если нужна иконка
 
-number_1 = Entry(root, width=12)  # Entry ввести одну строку текста
+number_1 = Entry(root, width=17)  # Entry ввести одну строку текста
 number_1.grid(column=1, row=0, columnspan=2, sticky=W)
-number_2 = Entry(root, width=12)  # Entry ввести одну строку текста
+number_2 = Entry(root, width=17)  # Entry ввести одну строку текста
 number_2.grid(column=1, row=3, columnspan=2, sticky=W)
 
 Label(root, text='Введи число A').grid(column=0, row=0)
@@ -25,19 +25,19 @@ Label(root, text='Б x 2.9 % =').grid(column=0, row=6)
 Label(root, text='Сумма А + Б').grid(column=0, row=7)
 Label(root, text='(А+Б) x 0,2 % =').grid(column=0, row=8)
 
-result_10 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_10 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_10.grid(column=1, row=1)
-result_5 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_5 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_5.grid(column=1, row=2)
-result_22 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_22 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_22.grid(column=1, row=4)
-result_51 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_51 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_51.grid(column=1, row=5)
-result_29 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_29 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_29.grid(column=1, row=6)
-result_1_2 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_1_2 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_1_2.grid(column=1, row=7)
-result_02 = Label(root, text=[], font=('Arial', 9, 'bold'))
+result_02 = Label(root, text='', font=('Arial', 9, 'bold'))
 result_02.grid(column=1, row=8)
 
 
@@ -113,14 +113,14 @@ def performE(self):
 def delet():
     number_1.delete(0, END)
     number_2.delete(0, END)
-    result_10.config(text=[])
-    result_5.config(text=[])
-    result_22.config(text=[])
-    result_51.config(text=[])
-    result_10.config(text=[])
-    result_29.config(text=[])
-    result_1_2.config(text=[])
-    result_02.config(text=[])
+    result_10.config(text='')
+    result_5.config(text='')
+    result_22.config(text='')
+    result_51.config(text='')
+    result_10.config(text='')
+    result_29.config(text='')
+    result_1_2.config(text='')
+    result_02.config(text='')
 
 
 def deletE(self):
@@ -130,12 +130,12 @@ def deletE(self):
 root.bind('<Return>', performE)
 root.bind('<Delete>', deletE)
 
-Button(root, text='learn', command=perform,
+Button(root, text='расчитать', command=perform, bg='green',
        font=('Arial', 9, 'bold'),
-       height=1, width=5).grid(column=1, row=9)
+       height=1, width=8).grid(column=1, row=9)
 
-Button(root, text='del', command=delet,
+Button(root, text='очистить', command=delet, bg='yellow',
        font=('Arial', 9, 'bold'),
-       height=1, width=5).grid(column=2, row=9)
+       height=1, width=8).grid(column=0, row=9)
 
 root.mainloop()
